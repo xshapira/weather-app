c7ab0fdec7d30f1c808c1b9df7d55de749a86e12
Max Shapira <maxshapira9@gmail.com>
2020-11-23 12:10:19 -0600

    create first API request

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,15 @@
 const key = '6XWKlaz3OAJ1xln60n6Tgfj2Wa2te6Ah';
+
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('manchester')
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
